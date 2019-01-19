7f35cdccb67b313db4aae83c602e7107a67f87e7
RyanTWJ <ghost_heartbeat@yahoo.com.sg>
2019-01-19 17:22:22 +0800

    Initial Coding

--- Hack & Roll 2019 AR Trail/Assets/Hack & Roll/Scripts/HNRARController.cs
+++ Hack & Roll 2019 AR Trail/Assets/Hack & Roll/Scripts/HNRARController.cs
@@ -33,4 +33,99 @@ public class HNRARController : MonoBehaviour
   /// Stores all detecected Augmented Images
   /// </summary>
   private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+
+  private bool m_IsQuitting = false;
+
+  private void Update()
+  {
+    _UpdateApplicationLifecycle();
+
+    TrackAllDetectedPlanes();
+
+    if (m_AllPlanes.Count <= 0)
+    {
+      SearchingForPlaneUI.SetActive(true);
+      return;
+    }
+    SearchingForPlaneUI.SetActive(false);
+
+    FindAugmentedImages();
+
+    // TODO: Find a plane, the image, spawn a prefab arrow to store 3d positions
+  }
+
+  private void FindAugmentedImages()
+  {
+    Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+  }
+
+  private void TrackAllDetectedPlanes()
+  {
+    Session.GetTrackables<DetectedPlane>(m_AllPlanes);
+  }
+
+  #region MISC
+  /// <summary>
+  /// Check and update the application lifecycle.
+  /// </summary>
+  private void _UpdateApplicationLifecycle()
+  {
+    // Exit the app when the 'back' button is pressed.
+    if (Input.GetKey(KeyCode.Escape))
+    {
+      Application.Quit();
+    }
+
+    // Only allow the screen to sleep when not tracking.
+    if (Session.Status != SessionStatus.Tracking)
+    {
+      const int lostTrackingSleepTimeout = 15;
+      Screen.sleepTimeout = lostTrackingSleepTimeout;
+    }
+    else
+    {
+      Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    if (m_IsQuitting)
+    {
+      return;
+    }
+
+    // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+    if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+    {
+      _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+      m_IsQuitting = true;
+      Invoke("_DoQuit", 0.5f);
+    }
+    else if (Session.Status.IsError())
+    {
+      _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+      m_IsQuitting = true;
+      Invoke("_DoQuit", 0.5f);
+    }
+  }
+
+  /// <summary>
+  /// Show an Android toast message.
+  /// </summary>
+  /// <param name="message">Message string to show in the toast.</param>
+  private void _ShowAndroidToastMessage(string message)
+  {
+    AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+    if (unityActivity != null)
+    {
+      AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+      unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+      {
+        AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+            message, 0);
+        toastObject.Call("show");
+      }));
+    }
+  }
+  #endregion
 }
