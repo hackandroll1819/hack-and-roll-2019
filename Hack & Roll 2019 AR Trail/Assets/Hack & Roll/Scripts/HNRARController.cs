dd69ebb5ae0dedc0ae9a9ef5a1922ddec437d0ac
brandonyeoxg <brandonyeoxg@gmail.com>
2019-01-20 03:14:47 +0800

    Add navigating pane

--- Hack & Roll 2019 AR Trail/Assets/Hack & Roll/Scripts/HNRARController.cs
+++ Hack & Roll 2019 AR Trail/Assets/Hack & Roll/Scripts/HNRARController.cs
@@ -6,200 +6,209 @@ using UnityEngine.UI;
 
 public class HNRARController : MonoBehaviour
 {
-  /// <summary>
-  /// The first-person camera being used to render the passthrough camera image (i.e. AR background).
-  /// </summary>
-  public Camera FirstPersonCamera;
+    /// <summary>
+    /// The first-person camera being used to render the passthrough camera image (i.e. AR background).
+    /// </summary>
+    public Camera FirstPersonCamera;
 
-  public GameObject[] ToSpawnPrefabs;
+    public GameObject[] ToSpawnPrefabs;
 
-  /// <summary>
-  /// A game object parenting UI for displaying the "searching for planes" snackbar.
-  /// </summary>
-  //public GameObject SearchingForPlaneUI;
+    /// <summary>
+    /// A game object parenting UI for displaying the "searching for planes" snackbar.
+    /// </summary>
+    //public GameObject SearchingForPlaneUI;
 
-  /// <summary>
-  /// A list to hold all planes ARCore is tracking in the current frame. This object is used across
-  /// the application to avoid per-frame allocations.
-  /// </summary>
-  private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
+    /// <summary>
+    /// A list to hold all planes ARCore is tracking in the current frame. This object is used across
+    /// the application to avoid per-frame allocations.
+    /// </summary>
+    private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
 
-  /// <summary>
-  /// The overlay containing the fit to scan user guide.
-  /// </summary>
-  //public GameObject FitToScanOverlay;
+    /// <summary>
+    /// The overlay containing the fit to scan user guide.
+    /// </summary>
+    //public GameObject FitToScanOverlay;
 
-  /// <summary>
-  /// Stores all detecected Augmented Images
-  /// </summary>
-  private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+    /// <summary>
+    /// Stores all detecected Augmented Images
+    /// </summary>
+    private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
 
-  public UIController UIController;
+    public UIController UIController;
 
-  private bool m_IsQuitting = false;
+    private bool m_IsQuitting = false;
 
-  private Anchor existingAnchor;
+    private Anchor existingAnchor;
 
-  private int currTrackingIdx = -1;
+    private int currTrackingIdx = -1;
 
-  private bool UserResponded = true;
+    private bool UserResponded = true;
 
-  private AugmentedImage nextAI = null;
+    private AugmentedImage nextAI = null;
 
-  private void Update()
-  {
-    if (m_AllPlanes.Count <= 0)
+    private bool hasScannedImage = false;
+
+    private void Update()
     {
-      UIController.ShowCalibratePrompt();
-    }
-    else
-    {
-      //_ShowAndroidToastMessage(m_TempAugmentedImages.Count.ToString());
-      if (m_TempAugmentedImages.Count <= 0)
-      {
-        UIController.ShowScanImagePrompt();
-      }
-      // NOTE: This is for already scanned image
-      /*else
-      {
-        UIController.SetActive(false);
-      }*/
-      // SearchingForPlaneUI.SetActive(false);
-    }
+        if (m_AllPlanes.Count <= 0)
+        {
+            UIController.ShowCalibratePrompt();
+        }
+        else
+        {
+            //_ShowAndroidToastMessage(m_TempAugmentedImages.Count.ToString());
+            if (m_TempAugmentedImages.Count <= 0)
+            {
+                UIController.ShowScanImagePrompt();
+            }
+            // NOTE: This is for already scanned image
+            else
+            {
+                //UIController.SetActive(false);
+                //UIController.ShowNavigatingScreen();
+                hasScannedImage = true;
+            }
 
-    _UpdateApplicationLifecycle();
+            if (hasScannedImage)
+            {
+                UIController.ShowNavigatingScreen();
+            }
+            // SearchingForPlaneUI.SetActive(false);
+        }
 
-    TrackAllDetectedPlanes();
+        _UpdateApplicationLifecycle();
 
-    if (m_AllPlanes.Count <= 0)
-    {
-      UIController.ShowCalibratePrompt();
-      return;
-    }
-    // UIController.SetActive(false);
+        TrackAllDetectedPlanes();
 
-    FindAugmentedImages();
+        if (m_AllPlanes.Count <= 0)
+        {
+            UIController.ShowCalibratePrompt();
+            return;
+        }
+        // UIController.SetActive(false);
 
-    // TODO: Include a way to track which augmented image is active
+        FindAugmentedImages();
 
-    if (!UserResponded)
-    {
-      return;
-    }
+        // TODO: Include a way to track which augmented image is active
 
-    foreach (AugmentedImage AI in m_TempAugmentedImages)
-    {
-      if (currTrackingIdx < 0)
-      {
-        ReinstantiateRoute(AI);
-        break;
-      }
-      // TODO: Prompt: handle the different database index change
-      /*else if (AI.DatabaseIndex != currTrackingIdx)
-      {
-        // TODO: Prompt User for change
-        //_ShowAndroidToastMessage("Prompt User");
-        nextAI = AI;
-        UserResponded = false;
-      }*/
-    }
-  }
+        if (!UserResponded)
+        {
+            return;
+        }
 
-  public void UserResponse(bool yo)
-  {
-    if (yo)
-    {
-      Destroy(existingAnchor.gameObject);
-      ReinstantiateRoute(nextAI);
-      nextAI = null;
-    }
-    // TODO: Close Pop up
-    UserResponded = true;
-  }
-
-  private void ReinstantiateRoute(AugmentedImage AI)
-  {
-    _ShowAndroidToastMessage("Detected picture, showing route!");
-    if (AI.TrackingState == TrackingState.Tracking)
-    {
-      existingAnchor = m_AllPlanes[AI.DatabaseIndex].CreateAnchor(AI.CenterPose);
-      Instantiate(ToSpawnPrefabs[AI.DatabaseIndex], AI.CenterPose.position, AI.CenterPose.rotation, existingAnchor.transform);
-      currTrackingIdx = AI.DatabaseIndex;
-    }
-  }
-
-  private void FindAugmentedImages()
-  {
-    Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
-  }
-
-  private void TrackAllDetectedPlanes()
-  {
-    Session.GetTrackables<DetectedPlane>(m_AllPlanes);
-  }
-
-  #region MISC
-  /// <summary>
-  /// Check and update the application lifecycle.
-  /// </summary>
-  private void _UpdateApplicationLifecycle()
-  {
-    // Exit the app when the 'back' button is pressed.
-    if (Input.GetKey(KeyCode.Escape))
-    {
-      Application.Quit();
+        foreach (AugmentedImage AI in m_TempAugmentedImages)
+        {
+            if (currTrackingIdx < 0)
+            {
+                ReinstantiateRoute(AI);
+                break;
+            }
+            // TODO: Prompt: handle the different database index change
+            /*else if (AI.DatabaseIndex != currTrackingIdx)
+            {
+              // TODO: Prompt User for change
+              //_ShowAndroidToastMessage("Prompt User");
+              nextAI = AI;
+              UserResponded = false;
+            }*/
+        }
     }
 
-    // Only allow the screen to sleep when not tracking.
-    if (Session.Status != SessionStatus.Tracking)
+    public void UserResponse(bool yo)
     {
-      const int lostTrackingSleepTimeout = 15;
-      Screen.sleepTimeout = lostTrackingSleepTimeout;
-    }
-    else
-    {
-      Screen.sleepTimeout = SleepTimeout.NeverSleep;
-    }
-
-    if (m_IsQuitting)
-    {
-      return;
+        if (yo)
+        {
+            Destroy(existingAnchor.gameObject);
+            ReinstantiateRoute(nextAI);
+            nextAI = null;
+        }
+        // TODO: Close Pop up
+        UserResponded = true;
     }
 
-    // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
-    if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+    private void ReinstantiateRoute(AugmentedImage AI)
     {
-      _ShowAndroidToastMessage("Camera permission is needed to run this application.");
-      m_IsQuitting = true;
-      Invoke("_DoQuit", 0.5f);
+        _ShowAndroidToastMessage("Detected picture, showing route!");
+        if (AI.TrackingState == TrackingState.Tracking)
+        {
+            existingAnchor = m_AllPlanes[AI.DatabaseIndex].CreateAnchor(AI.CenterPose);
+            Instantiate(ToSpawnPrefabs[AI.DatabaseIndex], AI.CenterPose.position, AI.CenterPose.rotation, existingAnchor.transform);
+            currTrackingIdx = AI.DatabaseIndex;
+        }
     }
-    else if (Session.Status.IsError())
-    {
-      _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
-      m_IsQuitting = true;
-      Invoke("_DoQuit", 0.5f);
-    }
-  }
 
-  /// <summary>
-  /// Show an Android toast message.
-  /// </summary>
-  /// <param name="message">Message string to show in the toast.</param>
-  private void _ShowAndroidToastMessage(string message)
-  {
-    AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-    AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-    if (unityActivity != null)
+    private void FindAugmentedImages()
     {
-      AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
-      unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
-      {
-        AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
-            message, 0);
-        toastObject.Call("show");
-      }));
+        Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
     }
-  }
-  #endregion
+
+    private void TrackAllDetectedPlanes()
+    {
+        Session.GetTrackables<DetectedPlane>(m_AllPlanes);
+    }
+
+    #region MISC
+    /// <summary>
+    /// Check and update the application lifecycle.
+    /// </summary>
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+              message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+    #endregion
 }
