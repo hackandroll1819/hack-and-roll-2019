08951bb3454596cfe448ee90d4bbebc711030dc0
RyanTWJ <ghost_heartbeat@yahoo.com.sg>
2019-01-20 11:29:48 +0800

    Arrow Interactable Script

---
+++ Hack & Roll 2019 AR Trail/Assets/Hack & Roll/Scripts/ArrowInteractable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowInteractable : MonoBehaviour
+{
+  public ParticleSystem ParticleSystem;
+
+  private void Start()
+  {
+    ParticleSystem.gameObject.SetActive(false);
+  }
+
+  private void OnMouseUp()
+  {
+    if (!ParticleSystem.gameObject.activeSelf)
+    {
+      ParticleSystem.gameObject.SetActive(true);
+      StartCoroutine(OffPS());
+    }
+  }
+
+  private IEnumerator OffPS()
+  {
+    yield return new WaitForSeconds(ParticleSystem.main.duration);
+    ParticleSystem.gameObject.SetActive(false);
+  }
+}
